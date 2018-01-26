9ef7ced5c2d37c9feb09575e82699d8e9cad7a93
rahul05ranjan <rahulsingh.ranjan06@gmail.com>
2018-01-26 15:02:03 +0530

    Add files via upload

---
+++ upload.php
@@ -0,0 +1,43 @@
+<?php
+if(isset($_POST["submit"])){
+    $check = getimagesize($_FILES["image"]["tmp_name"]);
+    print_r($check);
+    die();
+    if($check !== false){
+        $image = $_FILES['image']['tmp_name'];
+        $imgContent = addslashes(file_get_contents($image));
+        print_r($imgContent);
+        die();
+
+        /*
+         * Insert image data into database
+         */
+        
+        //DB details
+        $dbHost     = 'localhost';
+        $dbUsername = 'root';
+        $dbPassword = '';
+        $dbName     = 'test';
+        
+        //Create connection and select DB
+        $db = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
+        
+        // Check connection
+        if($db->connect_error){
+            die("Connection failed: " . $db->connect_error);
+        }
+        
+        $dataTime = date("Y-m-d H:i:s");
+        
+        //Insert image content into database
+        $insert = $db->query("INSERT into images (image, created) VALUES ('$imgContent', '$dataTime')");
+        if($insert){
+            echo "File uploaded successfully.";
+        }else{
+            echo "File upload failed, please try again.";
+        } 
+    }else{
+        echo "Please select an image file to upload.";
+    }
+}
+?>
